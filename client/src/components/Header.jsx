ac25d0e88e93d66f482f8a2bdb44bbed05b90497
RAJA PANDEY <92283721+Rpandey010@users.noreply.github.com>
2024-02-24 12:49:36 +0530

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,12 +7,12 @@
 //     return (
 //         <header className="bg-slate-200 shadow-md">
 //             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-//                 <Link to="/">
-//                     <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-//                         <span className="text-slate-500">Furni</span>
-//                         <span className="text-rose-500">Hub</span>
-//                     </h1>
-//                 </Link>
+                // <Link to="/">
+                //     <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                //         <span className="text-slate-500">Furni</span>
+                //         <span className="text-rose-500">Hub</span>
+                //     </h1>
+                // </Link>
 
 //                 <form className="bg-slate-100 p-3 rounded-lg flex items-center">
 //                     <input
@@ -51,7 +51,7 @@
 // }
 
 
-// ---------------------------------git---------------------------------
+// ---------------------------------git (aove code has some errors)---------------------------------
 
 import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
@@ -80,12 +80,12 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-          </h1>
-        </Link>
+      <Link to="/">
+                    <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                        <span className="text-slate-500">Furni</span>
+                        <span className="text-rose-500">Hub</span>
+                    </h1>
+                </Link>
         <form
           onSubmit={handleSubmit}
           className='bg-slate-100 p-3 rounded-lg flex items-center'
@@ -129,6 +129,3 @@ export default function Header() {
   );
 }
 
-
-//raja
-//pandey
