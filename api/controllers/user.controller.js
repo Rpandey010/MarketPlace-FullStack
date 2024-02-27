91a6476986b884e487e79faefd749834fa3fc3cd
Rishabh2333g <rishabh.gupta21@st.niituniversity.in>
2024-02-27 21:31:42 +0530

    User Riute api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,32 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error';
+
 export const test = (req, res) => {
-    res.json({ message: 'Jay shree ram' });
+    res.json({ message: 'Jay shree ram' 
+});
+};
+
+export const updateUser = async (req, res, next) => {
+    if(req.user._id !== req.params.id) return next(errorHandler(401, "You can only update your own account!"))
+    try{
+if(req.body.password) {
+    req.body.password = bcryptjs.hashSync(req.body.password, 10)
+}
+
+const updateUser = await User.findByIdAndUpdate(req.params.id, {
+    $set:{
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar,
+    }
+    }, {new: true})
+
+    const {password, ...rest} = updateUser._doc;
+
+    res.status(200).json(rest);
+} catch(error) {
+    return next(error)
+}
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/test', test);
-
+router.post('/update/:id', verifyToken, updateUser)
 
 export default router;
