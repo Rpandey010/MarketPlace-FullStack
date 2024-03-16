2499651a69cb6a38616fd2237452ee80b2a9b72d
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-03-16 22:21:38 +0530

    Create update user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,32 +1,30 @@
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error';
+import { errorHandler } from '../utils/error.js';
 
 export const test = (req, res) => {
-    res.json({ message: 'Jay shree ram' 
-});
+    res.json({ message: 'Jay shree ram' });
 };
 
-export const updateUser = async (req, res, next) => {
-    if(req.user._id !== req.params.id) return next(errorHandler(401, "You can only update your own account!"))
-    try{
-if(req.body.password) {
-    req.body.password = bcryptjs.hashSync(req.body.password, 10)
+export const updateUser = async(req, res, next) => {
+    if(req.user.id !== req.params.id) 
+        return next(errorHandlerrHandler(401, "You can only update your own account" ))
+    try {
+        if (req.body.password) {
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, {new: true})
+        
+        const {password, ...rest} = updatedUser._doc
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error)
 }
-
-const updateUser = await User.findByIdAndUpdate(req.params.id, {
-    $set:{
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar,
     }
-    }, {new: true})
-
-    const {password, ...rest} = updateUser._doc;
-
-    res.status(200).json(rest);
-} catch(error) {
-    return next(error)
-}
-};
