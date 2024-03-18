f57af2da9c880537899bc9334eb9d1fd10bb3c1e
Rishabh Gupta <156583510+Rishabh2333g@users.noreply.github.com>
2024-03-18 18:27:14 +0530

    SignOut Function

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -47,9 +47,35 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
           },
+          
+          signOutUserStart: (state) => {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        signOutUserFailure: (state, action) => {
+                state.error = action.payload;
+                state.loading = false;
     }
+  }
 });
 
-export const { signInStart, signInSuccess, signInFailure, updateUserFailure, updateUserStart, updateUserSuccess, deleteUserFailure, deleteUserSuccess, deleteUserStart,} = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
+} = userSlice.actions;
 
 export default userSlice.reducer;
