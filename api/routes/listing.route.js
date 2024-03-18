e795a323ba782089fbcc6a08858c953e5b12f8f5
Rishabh Gupta <156583510+Rishabh2333g@users.noreply.github.com>
2024-03-18 19:59:25 +0530

    Update listing.route.js

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,9 +5,5 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
-router.delete('/delete/:id', verifyToken, deleteListing);
-router.post('/update/:id', verifyToken, updateListing);
-router.get('/get/:id', getListing);
-router.get('/get', getListings);
 
 export default router;
