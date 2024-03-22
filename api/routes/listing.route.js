f7caf9d52e61e26bdb6210ecf23c13db85eede2c
Rishabh Gupta <156583510+Rishabh2333g@users.noreply.github.com>
2024-03-22 21:07:47 +0530

    Adding delete function

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing } from '../controllers/listing.contoller.js';
+import { createListing, deleteListing} from '../controllers/listing.contoller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
